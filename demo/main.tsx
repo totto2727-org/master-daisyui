ce005b6d34a1279de24fa7c65241bd974089c7c4
totto2727 <h_tsuchida@yumemi.co.jp>
2024-07-18 19:03:31 +0900

    docs(improve): update demo
    - Exclude executables from static file delivery
    - Remove unwanted Preloads
    - Apply formatting
    - Version Fixation

--- demo/main.tsx
+++ demo/main.tsx
@@ -2,22 +2,25 @@
 
 import { Hono } from "jsr:@hono/hono@4.5.0";
 import { logger } from "jsr:@hono/hono@4.5.0/logger";
-import { serveStatic } from 'jsr:@hono/hono@4.5.0/deno'
-import { cache } from 'jsr:@hono/hono@4.5.0/cache'
+import { serveStatic } from "jsr:@hono/hono@4.5.0/deno";
+import { cache } from "jsr:@hono/hono@4.5.0/cache";
 import { masterCssMiddleware } from "jsr:@totto/hono-mastercss@0.2.0";
-import * as path from "jsr:@std/path@1.0.0"
+import * as path from "jsr:@std/path@1.0.0";
 
-import masterDaisyUi, { dark, light } from "../mod.ts";
-import config from "./static/js/master.css.js"
+import masterDaisyUi from "../mod.ts";
+import config from "./static/js/master.css.js";
 
 const app = new Hono();
 
 app.use(logger());
 
-app.use("/content", masterCssMiddleware({
-  ...masterDaisyUi,
-  ...config
-}));
+app.use(
+  "/content",
+  masterCssMiddleware({
+    ...masterDaisyUi,
+    ...config,
+  }),
+);
 
 app.get("/content", (c) =>
   c.html(
@@ -25,16 +28,29 @@ app.get("/content", (c) =>
       <head>
         <meta charset="utf-8" />
         <title>Hello World!!!</title>
-        <link rel="preload" as="script" href="https://unpkg.com/htmx.org@2.0.0" />
+        <link
+          rel="preload"
+          as="script"
+          href="https://unpkg.com/htmx.org@2.0.0"
+        />
         <script src="https://unpkg.com/htmx.org@2.0.0"></script>
-        <link rel="preload" as="style" href="https://esm.sh/@master/normal.css@rc?css" />
-        <link rel="stylesheet" href="https://esm.sh/@master/normal.css@rc?css" />
-        <link rel="modulepreload" href="https://esm.sh/@master/css-runtime@rc" />
-        <link rel="modulepreload" href="./static/js/master.css.js" />
+        <link
+          rel="preload"
+          as="style"
+          href="https://esm.sh/@master/normal.css@rc?css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://esm.sh/@master/normal.css@rc?css"
+        />
+        <link
+          rel="modulepreload"
+          href="https://esm.sh/@master/css-runtime@rc"
+        />
         <link rel="modulepreload" href="./static/js/init-mastercss.js" />
         <script type="module" src="./static/js/init-mastercss.js"></script>
       </head>
-      <body class={`${light} ${dark} bg:base-100`}>
+      <body class="bg:base-100">
         <div class="flex flex-direction:column align-items:center">
           <div>Hello World!!!</div>
           <button hx-get="/api/content">
@@ -61,13 +77,19 @@ app.get(
 );
 
 app.use(
-  '/static/*',
+  "/static/*",
   cache({
-    cacheName: 'static',
-    cacheControl: 'max-age=86400',
+    cacheName: "static",
+    cacheControl: "max-age=86400",
     wait: true,
-  })
-)
-app.use('/static/*', serveStatic({ root: `${path.relative(".", import.meta.dirname ?? ".")}/static`, rewriteRequestPath: (path) => path.replace("/static", "") },));
+  }),
+);
+app.use(
+  "/static/*",
+  serveStatic({
+    root: `${path.relative(".", import.meta.dirname ?? ".")}/static`,
+    rewriteRequestPath: (path) => path.replace("/static", ""),
+  }),
+);
 
 Deno.serve(app.fetch);
--- demo/static/js/master.css.js
+++ demo/static/js/master.css.js
@@ -1,8 +1,8 @@
-/** @type {import('npm:@master/css@rc').Config} */
+/** @type {import('npm:@master/css@2.0.0-rc.44').Config} */
 export default {
-    defaultMode: false,
-    modes:{
-        light:"media",
-        dark:"media"
-    }
-}
+  defaultMode: false,
+  modes: {
+    light: "media",
+    dark: "media",
+  },
+};
