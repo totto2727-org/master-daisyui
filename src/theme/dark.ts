a570436ef983ea479e26cf432047bea49cdd276d
totto2727 <h_tsuchida@yumemi.co.jp>
2024-07-19 00:50:57 +0900

    fix: Unify to oklch from rgb

--- src/theme/dark.ts
+++ src/theme/dark.ts
@@ -6,12 +6,18 @@ const themeBase = {
   "primary": "oklch(65.69% 0.196 275.75)",
   "secondary": "oklch(74.8% 0.26 342.55)",
   "accent": "oklch(74.51% 0.167 183.61)",
-  "neutral": "#2a323c",
-  "neutral-content": "#A6ADBB",
-  "base-100": "#1d232a",
-  "base-200": "#191e24",
-  "base-300": "#15191e",
-  "base-content": "#A6ADBB",
+  // "neutral": "#2a323c", // original
+  "neutral": "oklch(31.38% 0.021 254.14)",
+  // "neutral-content": "#A6ADBB", // original
+  "neutral-content": "oklch(74.65% 0.022 264.44)",
+  // "base-100": "#1d232a", // original
+  "base-100": "oklch(25.33% 0.016 252.42)",
+  // "base-200": "#191e24", // original
+  "base-200": "oklch(23.26% 0.014 253.1)",
+  // "base-300": "#15191e", // original
+  "base-300": "oklch(21.15% 0.012 254.09)",
+  // base-content: #A6ADBB // original
+  "base-content": "oklch(74.65% 0.022 264.44)",
 } satisfies ThemeBase;
 
 const theme = createTheme("@dark", themeBase);
--- src/theme/light.ts
+++ src/theme/light.ts
@@ -7,12 +7,17 @@ const themeBase = {
   secondary: "oklch(69.71% 0.329 342.55)",
   "secondary-content": "oklch(98.71% 0.0106 342.55)",
   accent: "oklch(76.76% 0.184 183.61)",
-  neutral: "#2B3440",
-  "neutral-content": "#D7DDE4",
+  // neutral: "#2B3440", // original
+  neutral: "oklch(32.18% 0.025 255.7)",
+  // neutral-content: "#D7DDE4",
+  "neutral-content": "oklch(89.5% 0.012 252.1)",
   "base-100": "oklch(100% 0 0)",
-  "base-200": "#F2F2F2",
-  "base-300": "#E5E6E6",
-  "base-content": "#1f2937",
+  // base-200: #F2F2F2, // original
+  "base-200": "oklch(96.12% 0 0)",
+  // base-300: #E5E6E6, // original
+  "base-300": "oklch(92.42% 0.001 197.14)",
+  // base-content: #1f2937, // original
+  "base-content": "oklch(27.81% 0.03 256.85)",
 } satisfies ThemeBase;
 
 const theme = createTheme("@light", themeBase);
